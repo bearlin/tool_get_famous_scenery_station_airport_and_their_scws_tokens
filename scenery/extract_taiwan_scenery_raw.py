2d97abe5a91a0e0dcf72e80930fd225e203b78b9
BearLin <bear.lin.001@gmail.com>
2015-06-16 15:56:41 +0800

    Add Taiwan scenery

--- scenery/extract_taiwan_scenery_raw.py
+++ scenery/extract_taiwan_scenery_raw.py
@@ -14,15 +14,22 @@ rawfile = codecs.open(filename, 'r', encoding='utf-8')
 dumpfile = codecs.open(filename + ".dump", 'w', encoding='utf-8')
 
 # Feed the rawfile into findall(); it returns a list of all the found strings
-pattern = re.compile(">\S*</strong></span></h2>")
+pattern = re.compile(">[0-9]*\.[^0-9a-zA-Z]*</span></td>")
 strings = re.findall(pattern, rawfile.read())
 print "%r" % (strings)
 
 # Processing matched strings
 for string in strings:
   print "Processing:%s" % (string)
-  strtmp1 = string.lstrip('>')
-  strtmp2 = strtmp1.rstrip('</strong></span></h2>')
+  strtmp1 = string.rstrip('</span></td>').lstrip()
+  print "after strip end:%s" % (strtmp1)
+  dotIdx = strtmp1.index('.')
+  startIdx = dotIdx + 1
+  endIdx = len(strtmp1)
+  print "dotIdx:%r" % (dotIdx)
+  print "startIdx:%r" % (startIdx)
+  print "endIdx:%r" % (endIdx)
+  strtmp2 = strtmp1[startIdx:endIdx]
   strtmp3 = strtmp2.lstrip().rstrip() # strip white space characters
   if strtmp3 == '':
     continue
